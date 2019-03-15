ce173716e057730a3ef8f48ff9e2b6a918526cd2
丸山大幸 <>
2019-03-15 21:02:49 +0900

    fix: htmlの内容を変更

--- rakuten_report.py
+++ rakuten_report.py
@@ -18,8 +18,9 @@ import time
 RAKUTEN_APPLICATION_ID = "1096515298420576244"
 RAKUTEN_ICHIBA_API = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?"
 
-# イメージソースであることを示す文字列
+# コンバート用の目印
 IMG_SOURCE = "img_src: "
+LINK_SOURCE = "link_src: "
 
 # 商品情報インスタンス
 class Product:
@@ -86,23 +87,26 @@ def convert_table(products):
     return pd.DataFrame(data=rows, columns=Product.get_columns())
 
 # htmlファイルを保存する
-def save_html(date, keywords, img_price_hist, sales_info, sales_old_info):
+def save_html(date, keywords, img_price_hist, highers, lowers, links):
     source = codecs.open("test.md", mode="r", encoding="utf-8")
     text = source.read()
     html = markdown.markdown(text)
-    html = html.replace("DATE", date).replace("KEYWORDS", keywords).replace("PRICE_HIST", img_price_hist).replace("HIGH_PRICE_TOP5", sales_info).replace("LOW_PRICE_TOP5", sales_old_info)
+    html = html.replace("DATE", date).replace("KEYWORDS", keywords).replace("PRICE_HIST", img_price_hist).replace("HIGH_PRICE_TOP5", highers).replace("LOW_PRICE_TOP5", lowers)
     html = convert_imgTag(html)
+    html = merge_link_to_title(html, links)
     html_file = codecs.open("report.html", "w", encoding="utf-8", errors="xmlcharrefreplace")
     html_file.write(html)
     html_file.close()
 
 # イメージソースである印をつける
 def mark_imgSrc(path):
-    return IMG_SOURCE + path
-
+    return IMG_SOURCE + path if path != None else ""
+# タイトルである印をつける
+def mark_titleSrc(path):
+    return LINK_SOURCE + path if path != None else ""
 # <table>内のイメージソースを<img>タグにコンバートする
 def convert_imgTag(html):
-    soup = BeautifulSoup(html, "html")
+    soup = BeautifulSoup(html, "html.parser")
     for td in soup.find_all("td", text=re.compile(IMG_SOURCE)):
         src = td.text.replace(IMG_SOURCE, "")
         imgTag = soup.new_tag("img", src=src)
@@ -112,6 +116,20 @@ def convert_imgTag(html):
     
     return str(soup)
 
+# タイトルとリンクを1つのタグに束ねる
+def merge_link_to_title(html, links):
+    soup = BeautifulSoup(html, "html.parser")
+    from IPython.core.debugger import Pdb; Pdb().set_trace()
+    for td, link in zip(soup.find_all("td", text=re.compile(LINK_SOURCE)), links):
+        title = td.text.replace(LINK_SOURCE, "")
+        linkTag = soup.new_tag("a", href=link)
+        linkTag.string = title 
+        new_td = soup.new_tag("td")
+        new_td.insert(0, linkTag)
+        td.replace_with(new_td)
+
+    return str(soup)
+
 # 商品ページをスクレイピングして楽天ポイントを取得
 def scraping_RakutenPoint(url):
     options = Options()
@@ -119,7 +137,11 @@ def scraping_RakutenPoint(url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     soup = BeautifulSoup(driver.page_source.encode("utf-8"), "html.parser")
-    text = soup.find("div", class_ = "point-summary__total___3rYYD").find("span").text.replace(",","") # コンマ排除
+    point_summary = soup.find("div", class_ = "point-summary__total___3rYYD")
+    if point_summary == None:
+        return 0
+    text = point_summary.find("span").text.replace(",","") # コンマ排除
+    
     return int(text)
 
 # 楽天市場レポートを作成する
@@ -144,14 +166,22 @@ def make_rakuten_report(keyword):
     # imgタグにコンバート
     table_high["image"] = table_high["image"].apply(mark_imgSrc)
     table_low["image"] = table_low["image"].apply(mark_imgSrc)
+    # タイトル情報を付与する
+    table_high["title"] = table_high["title"].apply(mark_titleSrc)
+    table_low["title"] = table_low["title"].apply(mark_titleSrc)
     # 楽天ポイントを算出
     table_high["point"] = table_high["link"].apply(scraping_RakutenPoint)
     table_low["point"] = table_low["link"].apply(scraping_RakutenPoint)
+    # リンク情報を取り出す
+    table_high_link = table_high.pop("link")
+    table_low_link = table_low.pop("link")
 
     table_high = table_high.to_html(index=None).replace("\n", "")
     table_low = table_low.to_html(index=None).replace("\n", "")
     date = datetime.date.today().strftime("%Y-%m-%d")
-    save_html(date, keyword, SRC_PRICE_HIST, table_high, table_low)
+    links = table_high_link.tolist()
+    links.extend(table_low_link.tolist())
+    save_html(date, keyword, SRC_PRICE_HIST, table_high, table_low, links)
 
 def main():
     print("検索キーワードを入力してください")
