d8d3235b63ac37153276f679cc515270c2f604cc
丸山大幸 <>
2019-03-17 21:15:12 +0900

    add: 市場解析クラスを追加

---
+++ marketAnalyzer.py
@@ -0,0 +1,31 @@
+from data import Product
+from data import MarketData
+from matplotlib import pyplot as plt
+import pandas as pd
+
+# ランキングの表示数
+RANK = 5
+# ヒストグラムの保存先
+SRC_PRICE_HIST = "price_hist.png"
+
+class MarketAnalyzer:
+
+#-- class method --
+    # 商品情報から市場解析結果を出力する
+    def analyze(self, products):
+
+        if products == None or len(products) < 1:
+            return None
+        table = self.__convert_table(products)
+        table["price"].hist(bins=100)
+        plt.savefig(SRC_PRICE_HIST)
+        higher = table.sort_values(by=["price"], ascending=False).head(RANK)
+        lower = table.sort_values(by=["price"]).head(RANK)
+        
+        return MarketData(SRC_PRICE_HIST, higher, lower)
+
+#-- private method --    
+    # 商品情報をテーブル化する
+    def __convert_table(self, products):
+        rows = list(map(lambda product: product.get_row(), products))
+        return pd.DataFrame(data=rows, columns=Product.get_columns())
